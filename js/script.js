417438361b4606b2b1cd2d893839c3f396aa0b8e
Ayokunle <Ayoonadiji@gmail.com>
2021-03-20 09:43:47 +0100

    initial

---
+++ js/script.js
@@ -0,0 +1,53 @@
+let humanScore = 0;
+let computerScore = 0;
+let currentRoundNumber = 1;
+
+var humGuess = document.getElementById("human-guess");
+humGuess.addEventListener("input", validate);
+
+// Write your code below:
+// function validate(){
+//     if(document.getElementById("human-guess").value > 9){
+//     } return alert("Number is out of range");
+
+// }
+// validate();
+
+
+function generateTarget(){
+    const randomNumber = Math.floor(Math.random() * 9);
+    return randomNumber;
+}
+
+function compareGuesses(hum, com, sec){
+if ((Math.abs(hum - sec) < Math.abs(com - sec)) || Math.abs(hum - sec)==Math.abs(com - sec)){
+    return true;
+}else {
+    return false;
+}
+}
+
+function updateScore(str){
+    switch (str) {
+        case "human":
+            humanScore++;
+            break;
+    
+        default:
+            computerScore++;
+            break;
+    }
+}
+
+function advanceRound(){
+    currentRoundNumber++;
+}
+
+function validate() {
+    if(humGuess.value > 9 || humGuess.value < 0 || humGuess.value == ""){
+        humGuess.value = 0;
+        alert("Number is out of range"); 
+    }   else if (humGuess.value == null){
+        alert("Enter a number"); 
+    }
+}
